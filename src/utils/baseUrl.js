f1aab690c10f741091708e490803d364ca8f0d86
MD Nadim Rijvi Shakib <84374961+nrshakib@users.noreply.github.com>
2025-07-26 14:46:24 +0600

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,9 +1,11 @@
 export const getBaseUrl = () => {
-  return "https://humayon5003.binarybards.online/api/v1";
+  return "http://3.141.148.237:5003/api/v1";
+  // return "https://humayon5003.binarybards.online/api/v1";
   // return "http://10.0.70.35:8084/api/v1";
 };
 
 export const getImageUrl = () => {
-  return "https://humayon5003.binarybards.online/";
+  return "http://3.141.148.237:5003/";
+  // return "https://humayon5003.binarybards.online/";
   // return "http://10.0.70.35:8084";
 };
